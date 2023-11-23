8d2db6e168668ab580b5b28468fba0b7133c3c43
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2023-11-23 10:25:19 -0300

    LocalStorage de usuario logueado

--- src/providers/UserContext.jsx
+++ src/providers/UserContext.jsx
@@ -1,19 +1,24 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  })
 
   const loginUser = (newUser) => {
     setUser(newUser);
   };
 
-  const isLoggedIn = () => {
-    return user ? true : false
-  }
-
   const logoutUser = () => {
     setUser(null)
   }
+
+  // Local Storage Update
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
+
 
   return (
     <UserContext.Provider value={{ user, loginUser, logoutUser }}>
